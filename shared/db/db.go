a8cee8ef7dfca2dfa4a71b8e1b1ee52140fe1baa
htoyoda18 <h.toyoda@attivita.co.jp>
2022-09-10 00:58:46 +0900

    Add: dockerでmySQLの環境を構築

--- shared/db/db.go
+++ shared/db/db.go
@@ -11,8 +11,17 @@ import (
 )
 
 func InitDB(conf config.Configuration) (*gorm.DB, *sql.DB, error) {
-	dbConnection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true&loc=Local", conf.DBUserTest, conf.DBPassTest, conf.DBHost, conf.DBNameTest)
-	db, err := gorm.Open(mysql.Open(dbConnection), &gorm.Config{})
+	user := "webuser"
+	password := "webpass"
+	host := "localhost"
+	port := "3306"
+	database_name := "go_mysql8_development"
+
+	dbconf := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4"
+
+	// dbConnection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true&loc=Local", conf.DBUserTest, conf.DBPassTest, conf.DBHost, conf.DBNameTest)
+	fmt.Println(dbconf)
+	db, err := gorm.Open(mysql.Open(dbconf), &gorm.Config{})
 	if err != nil {
 		return nil, nil, err
 	}
